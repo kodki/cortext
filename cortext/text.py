0b49eccb9681c9c3654a633eca639a6f06e75050
SophiaPetrova <sophia5rova@gmail.com>
2015-10-03 18:56:30 +0300

    add classes text and visualizer and update requirements

---
+++ cortext/text.py
@@ -0,0 +1,10 @@
+import nltk
+
+class Text:
+    def __init__(self, raw_text):
+        self.raw_text = raw_text
+
+    def parse(self):
+        sentences = nltk.sent_tokenize(self.raw_text)
+        tokens = [nltk.word_tokenize(sentence) for sentence in sentences]
+        return [nltk.pos_tag(words) for words in tokens]
