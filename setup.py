1f394450d9b56e93977a91b5eb31df98d069a500
SophiaPetrova <sophia5rova@gmail.com>
2015-10-04 13:34:58 +0300

    v0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'cortext',
   packages = ['cortext'], 
-  version = '0.4',
+  version = '0.5',
   description = 'A text visualization library',
   author = 'Alexander Ivanov, Sophia Petrova',
   author_email = 'alehander42@gmail.com',
