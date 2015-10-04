5fe110b5e4414b5c9395481e11f6adbe0a613465
SophiaPetrova <sophia5rova@gmail.com>
2015-10-04 03:16:59 +0300

    add word name to word types

--- cortext/word_types.py
+++ cortext/word_types.py
@@ -2,60 +2,70 @@ class WordType:
     pass
 
 class Noun(WordType):
-    def __init__(self, image_url=None):
+    def __init__(self, word, image_url=None):
+        self.word = word
         self.image_url = image_url
 
 class Organization(WordType):
-    def __init__(self, abstract='', logo_url=None):
+    def __init__(self, word, abstract='', logo_url=None):
+        self.word = word
         self.abstract = abstract
         self.logo_url = logo_url
 
 class Celebrity(WordType):
-    def __init__(self, abstract='', image_url=None):
+    def __init__(self, word, abstract='', image_url=None):
+        self.word = word
         self.abstract = abstract
         self.image_url = image_url
 
 class Musician(WordType):
-    def __init__(self, abstract='', playlist_url=None, image_url=None):
+    def __init__(self, word, abstract='', playlist_url=None, image_url=None):
+        self.word = word
         self.abstract = abstract
         self.playlist_url = playlist_url
         self.image_url
 
 class Actor(WordType):
-    def __init__(self, abstract='', image_url=None, imdb_url=None):
+    def __init__(self, word, abstract='', image_url=None, imdb_url=None):
+        self.word = word
         self.abstract = abstract
         self.image_url = image_url
         self.imdb_url = imdb_url
 
 class Event(WordType):
-    def __init__(self, abstract='', time=None):
+    def __init__(self, word, abstract='', time=None):
+        self.word = word
         self.abstract = abstract
         self.time = time
 
 class Location(WordType):
-    def __init__(self, abstract='', map_url=None):
+    def __init__(self, word, abstract='', map_url=None):
+        self.word = word
         self.abstract = abstract
         self.map_url = map_url
 
 class Verb(WordType):
-    def __init__(self, image_url=None):
+    def __init__(self, word, image_url=None):
+        self.word = word
         self.image_url = image_url
 
 class Adjective(WordType):
-    def __init__(self, image_url=None):
+    def __init__(self, word, image_url=None):
+        self.word = word
         self.image_url = image_url
 
 class Color(WordType):
-    def __init__(self):
-        pass
-
+    def __init__(self, word):
+        self.word = word
+        
 class Link(WordType):
-    def __init__(self):
-        pass
+    def __init__(self, word):
+        self.word = word
         
 class Unknown(WordType):
-    def __init__(self):
-        pass
+    def __init__(self, word):
+        self.word = word
+        
 
 
 
