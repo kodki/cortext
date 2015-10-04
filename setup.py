7ec11fd03e973d6609175329e9bbb2939443e584
Alexander Ivanov <alehander42@gmail.com>
2015-10-04 12:49:56 +0300

    Prepare for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cortext',
+  packages = ['cortext'], 
+  version = '0.2',
+  description = 'A text visualization library',
+  author = 'Alexander Ivanov, Sophia Petrova',
+  author_email = 'alehander42@gmail.com',
+  url = 'https://github.com/kodki/cortext', 
+  download_url = 'https://github.com/kodki/cortext/tarball/0.2', 
+  keywords = ['natural', 'language'],
+  classifiers = [],
+)
